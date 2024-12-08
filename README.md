13c7f3f7d231292f8e0728293501987a2a630cd2
Muslim Helalee <muslim123helalee@gmail.com>
2024-12-08 17:02:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# User Profile App
+
+This is a simple user profile app built with React, TypeScript, and Tailwind CSS. The goal of this project is to help you understand how to update the UI of an app and how to handle external communication of React components.
+
+![User Profile App Screenshot](./public/Project-Screenshot.png)
+
+## Learning Objectives
+
+- Understanding how components communicate with external resources
+- Learning the useEffect hook
+- Understanding the differences between the useEffect and useState hooks
+- Learning how to render JSX conditionally
+- Using Typescript to safe guard incoming data
+
+## Features
+
+- Fetch user data from a public API (JSONPlaceholder).
+- Display user profile information (name, email, age).
+- Load the next user profile after clicking the "Next User" button.
+- Reset and start over after viewing 10 users.
+
+## Taking a Look at the Completed Project
+
+To set up the project after forking or downloading it from GitHub, follow these steps:
+
+1. Open with your favorite code editor
+
+2. Install the required dependencies by running the following command:
+
+```
+npm install
+```
+
+3. Start the development server by running the following command:
+
+```
+npm run dev
+```
+
+Happy Coding and Learning 😊
---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,64 @@
+interface User {
+  name: string;
+  email: string;
+  age: number;
+}
+
+interface UserProfileProps {
+  user: User;
+  nextUser: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export default function UserProfile({ user, nextUser }: UserProfileProps) {
+  return (
+    <form className="max-w-md mx-auto bg-white my-4 p-6 rounded-lg shadow-md">
+      <h2 className="text-xl font-semibold mb-4">Edit User Profile</h2>
+
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700 mb-1">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          className="w-full p-2 border border-gray-300 rounded"
+          value={user.name}
+          onChange={() => {}}
+        />
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700 mb-1">
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          className="w-full p-2 border border-gray-300 rounded"
+          value={user.email}
+          onChange={() => {}}
+        />
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700 mb-1">
+          Age
+        </label>
+        <input
+          type="number"
+          name="age"
+          className="w-full p-2 border border-gray-300 rounded"
+          value={user.age}
+          onChange={() => {}}
+        />
+      </div>
+
+      <button
+        onClick={nextUser}
+        className="bg-orange-500 hover:bg-orange-600 transition-colors text-white font-bold py-2 px-4 rounded"
+      >
+        Next User
+      </button>
+    </form>
+  );
+}
